6abc707bfb755c85eaf8bfb37ee74847691970ed
Otavio <otavio.nakai@escola.pr.gov.br>
2026-05-09 19:08:54 -0300

    Add files via upload

--- segundo projeto/java.js
+++ segundo projeto/java.js
@@ -3,9 +3,9 @@ const avanca = document.querySelectorAll('.btn-proximo');
 avanca.forEach(button => {
     button.addEventListener('click', function (){
         const atual = document.querySelector('.ativo');
-        const proximopasso = 'passo-' + this.getAttribute('data-proximo');
+        const proximopasso = 'passo-'+ this.getAttribute('data-proximo');
 
         atual.classList.remove('ativo');
-        document.getElementeByid(proximopasso).classList.add('ativo');
+        document.getElementById(proximopasso).classList.add('ativo');
         })
 })
